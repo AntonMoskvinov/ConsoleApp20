c184dd257bdc7e846254a4afb242ed64379e9fd9
AntonMoskvinov <128958172+AntonMoskvinov@users.noreply.github.com>
2023-11-03 15:34:19 +0200

    Update Program.cs

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<object> collection = new List<object>(); // Здесь нужно заполнить коллекцию элементами
+
+        Thread thread = new Thread(() =>
+        {
+            foreach (var item in collection)
+            {
+                // Вызываем метод ToString() для каждого элемента и выводим результат на экран
+                Console.WriteLine(item.ToString());
+            }
+        });
+
+        thread.Start();
+        thread.Join(); // Ждем завершения потока
+
+        Console.WriteLine("Главный поток завершил выполнение.");
+    }
+}
